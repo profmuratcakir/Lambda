e5fb29d5c245636e0c1c8df9768125a4d002598a
profmuratcakir <mcakiroglu.techproed@gmail.com>
2021-04-14 21:21:21 +0300

    day03

--- src/streamOrnekler/Stream01Integer.java
+++ src/streamOrnekler/Stream01Integer.java
@@ -19,22 +19,45 @@ public class Stream01Integer {
         rakamlar.add(-1000);
         rakamlar.add(4);
 
+        //**************************************************************************************
+        // ORNEK1: Bir listedeki tek sayıları yazdırınız.
+        //**************************************************************************************
         System.out.println("==== TEK SAYILAR ======");
         rakamlar.stream().filter(t -> t%2 != 0).forEach(x ->System.out.print(x));
 
+        //**************************************************************************************
+        // ORNEK2: Bir listedeki çift sayıları yazdırınız.
+        //**************************************************************************************
         System.out.println("\n==== ÇİFT SAYILAR ======");
-        rakamlar.stream().filter(t -> t%2 == 0).forEach(Stream01Integer::yazdır);
+        rakamlar.stream().filter(t -> t%2 == 0).forEach(Stream01Integer::yazdir);
 
+        //**************************************************************************************
+        // ORNEK3: Bir listedeki tek sayıları metot referansı ile yazdırınız.
+        //**************************************************************************************
         System.out.println("\n==== TEK SAYILAR =====");
-        rakamlar.stream().filter(Stream01Integer::tekMi).forEach(Stream01Integer::yazdır);
+        rakamlar.stream().filter(Stream01Integer::tekMi).forEach(Stream01Integer::yazdir);
 
+        //**************************************************************************************
+        // ORNEK4: Bir listedeki çift  sayıları  yazdıran bir metot tanımlayınız.
+        //**************************************************************************************
+        System.out.println("\n==== ÇİFT SAYILAR(METHOD) =====");
+        ciftleriYazdir(rakamlar);
     }
+
+    public static void ciftleriYazdir(List <Integer> liste){
+    //  liste.stream().filter(x->x%2==0).forEach(x-> System.out.print(x + " "));
+        liste.stream().filter(Stream01Integer::ciftMi).forEach(Stream01Integer::yazdir);
+    }
+
     public static boolean tekMi(int x){
         return x % 2 != 0;
     }
 
+    public static boolean ciftMi(int x){
+        return x%2==0;
+    }
 
-    public static void yazdır(int x){
+    public static void yazdir(int x){
         System.out.print( x + " ");
     }
 
---
+++ src/streamOrnekler/Stream02Integer.java
@@ -0,0 +1,45 @@
+package streamOrnekler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stream02Integer {
+
+    public static void main(String[] args) {
+        List<Integer> liste = new ArrayList<>();
+        liste.add(12);
+        liste.add(9);
+        liste.add(13);
+        liste.add(4);
+        liste.add(9);
+        liste.add(2);
+        liste.add(4);
+        liste.add(12);
+        liste.add(15);
+        tekKareYazdır(liste);
+        System.out.println("\nTOPLAM:" + tekKupToplami(liste));
+
+    }
+
+    //**************************************************************************************
+    // ORNEK5: Bir listedeki tek sayıların karelerini yazdıran bir metot tanımlayınız.
+    //**************************************************************************************
+    // map() metodu collection üzerinde bir transformation işlemi sağlayan ara işlem metodudur.
+    // Eğer bir collectionın verilerininin değişik hallerini hesaplama istersek map() kullanabiliriz.
+    public static void tekKareYazdır(List <Integer> l){
+       l.stream().distinct().filter(Stream01Integer::tekMi).map(x->x*x).forEach(Stream01Integer::yazdir);
+    }
+
+    //**************************************************************************************
+    // ORNEK6: Bir listedeki tek sayıların küplerinin toplamını hesaplayarak
+    // döndüren bir metot tanımlayınız.
+    //**************************************************************************************
+    // reduce() bir terminal işlemidir. Stream hattını kapatır ve tek bir sonuç üretir.
+    // Collection'ı indirgeme işlemini lambda fonksiyonu veya metot refransı ile yapabiliriz.
+    public static Integer tekKupToplami(List <Integer> l){
+    //  return l.stream().filter(Stream01Integer::tekMi).map(x->x*x*x).reduce(0,(x,y)-> (x+y));
+    //  return l.stream().filter(Stream01Integer::tekMi).map(x->x*x*x).reduce(Math::addExact);
+        return l.stream().filter(Stream01Integer::tekMi).map(x->x*x*x).reduce(0, Integer::sum);
+    }
+
+}
